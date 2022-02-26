391eb12f2628577fe36a6d32f2a9fb2cd80bb5ae
Andy Pan <panjf2000@gmail.com>
2022-02-27 00:01:07 +0800

    Update website for gnet v2

--- sidebars.js
+++ sidebars.js
@@ -4,9 +4,27 @@ module.exports = {
       type: 'category',
       label: 'About',
       items: [
-        "about/what-is-gnet",
-        "about/what-is-gnet-cn",
+        "about/overview",
+        "about/overview-cn",
       ],
     },
+    {
+      type: 'category',
+      label: 'Tutorial',
+      items: [
+        "tutorial/best-practices",
+      ],
+    },
+    'quickstart', 
+    'benchmark', 
+    {
+      type: 'category',
+      label: 'v1.x',
+      items: [
+        "v1.x/manual-for-gnet-v1",
+        "v1.x/manual-for-gnet-v1-cn",
+      ],
+    },
+    'faq', 
   ]
 };
